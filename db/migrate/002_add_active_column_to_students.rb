49478a9e4733108a5b1a76b8b347ecc693876f27
mstaso <mstaso1@gmail.com>
2020-06-23 17:34:23 -0400

    Done.

---
+++ db/migrate/002_add_active_column_to_students.rb
@@ -0,0 +1,6 @@
+class AddActiveColumnToStudents < ActiveRecord::Migration
+    def change
+        add_column :students, :active, :boolean, default: false
+        #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    end
+end
