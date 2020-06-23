ec7b66a1f6bdf08a11ef3f6a183edff65d267e90
afrolambo <stephennoahgalvan@.com>
2020-06-23 16:33:45 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active #boolean that we are calling from migration 002_add_active_column_to_student
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
